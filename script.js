241417bb517dfd2c96f45f46bc38f90fd5922deb
maryamabdi76 <maryamabdi9776@gmail.com>
2021-01-09 10:16:30 +0330

    calculator

---
+++ script.js
@@ -0,0 +1,6 @@
+
+function calcNumbers(result){
+    console.log(form.displayResult.value);
+    form.displayResult.value=form.displayResult.value+result;
+    console.log(form.displayResult.value);
+}
